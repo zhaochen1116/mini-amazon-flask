6fa83fecb67ba6ea0448056c09e0a278d4bff11b
zhaochen1116 <zhaochen1116@users.noreply.github.com>
2025-03-04 16:46:25 -0500

    Update recommend.py

--- recommend.py
+++ recommend.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 @app.route("/recommend/<user_id>", methods=["GET"])
 def recomment(user_id):
  recommended_products = ["Product A", "Product B", "Product C"]
- return jsonify({"user": user_id, "recommendatons": recommended_products})
+ return jsonify({"user": user_id, "recommendations": recommended_products})
 
-if __name__=="_main_":
- app.run(host="0.0.0.0", port=5001)
+if __name__ == "__main__":
+ print("Starting Flask server...")
+ app.run(host="0.0.0.0", port=5001, debug=True)
